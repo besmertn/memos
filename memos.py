b84935e00cb1e1e017e43df65b2e62d5eb1fdc0c
besmertn <besmertn@bk.ru>
2018-06-12 00:01:32 +0300

    repos created

---
+++ memos.py
@@ -0,0 +1,44 @@
+from flask import Flask
+import datetime
+from bot_handler import BotHandler
+
+app = Flask(__name__)
+
+token = '613303108:AAG4DDaG4tgkU1dsA1A5jA2rIQ_zDtmR3mc'
+greet_bot = BotHandler(token)
+greetings = ('здравствуй', 'привет', 'ку', 'здорово')
+now = datetime.datetime.now()
+
+
+def main():
+    new_offset = None
+    today = now.day
+    hour = now.hour
+
+    while True:
+        greet_bot.get_updates(new_offset)
+
+        last_update = greet_bot.get_last_update()
+
+        last_update_id = last_update['update_id']
+        last_chat_text = last_update['message']['text']
+        last_chat_id = last_update['message']['chat']['id']
+        last_chat_name = last_update['message']['chat']['first_name']
+
+        if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            today += 1
+
+        elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            today += 1
+
+        new_offset = last_update_id + 1
+
+
+if __name__ == '__main__':
+    app.run()
